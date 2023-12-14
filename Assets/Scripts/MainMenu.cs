d1aad512d3c52959adc160586eee65a0b4d6be1a
Reece <wraith0@hotmail.co.uk>
2023-12-14 21:16:40 +0000

    Main Menu and Functionality

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Level");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
